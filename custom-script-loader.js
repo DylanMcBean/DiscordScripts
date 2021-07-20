3e7bf7859bdeaa5f2f57ca3e97953949be06ccd2
Dylan McBean-Kyle <40377397+DylanMcBean@users.noreply.github.com>
2021-07-20 20:47:31 +0000

    Add files via upload

---
+++ custom-script-loader.js
@@ -0,0 +1,33 @@
+let fs = require("fs");
+let mainDir = "/custom-scripts/";
+let files = fs.readdirSync('.' + mainDir);
+let loaded = failed = 0;
+
+let successfulLoad = [
+    "color: #1ac",
+    "background-color: #000",
+    "padding: 2px 4px",
+    "border-radius: 2px",
+    "font-size: 20px"
+].join(';');
+
+let failedLoad = [
+    "color: #f05",
+    "background-color: #000",
+    "padding: 2px 4px",
+    "border-radius: 2px",
+    "font-size: 20px",
+    "font-weight: 900"
+].join(';');
+
+for (var file of files) {
+    try {
+        require('../../../' + mainDir + file);
+        loaded++;
+        console.log(`%cLoaded: ${file} successfully.`, successfulLoad);
+    } catch (Exception) {
+        loaded++;
+        console.log(`%c${file} failed to load. Reason: ${Exception}`, failedLoad);
+    }
+}
+console.log(`%cLoaded: ${loaded} scripts.\nFailed: ${failed} scripts.`, successfulLoad);
